523e2ca5461e42c338263cccc32794b6b1b37911
Benanna2019 <bass41992ben@gmail.com>
2022-09-12 20:45:41 -0400

    forgot to save blog layout component after removing props

--- src/components/Layouts/BlogLayout.tsx
+++ src/components/Layouts/BlogLayout.tsx
@@ -2,11 +2,11 @@ import BlogIndexPage from "../BlogIndexPage";
 import Footer from "../Footer";
 import Header from "../Header";
 
-export default function BlogLayout({ posts }: any) {
+export default function BlogLayout() {
   return (
     <>
       <Header />
-      <BlogIndexPage posts={posts} />
+      <BlogIndexPage />
       <Footer />
     </>
   );
